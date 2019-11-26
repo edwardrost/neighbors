129b9227103123c4f763426ba37300bac18018bd
edwardrost <edrost.pro@gmail.com>
2019-11-26 13:19:49 +0300

    Updates

---
+++ precache-manifest.3579b420fda7c5b5745542c7972db9ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a05f667962c9cfbbde4130d6fbcb84",
+    "url": "/github.io/neighbors/index.html"
+  },
+  {
+    "revision": "b91aa1df1d639274e4c7",
+    "url": "/github.io/neighbors/static/css/main.f95ad200.chunk.css"
+  },
+  {
+    "revision": "cda5e301d2fe1fb4d2e3",
+    "url": "/github.io/neighbors/static/js/2.257865db.chunk.js"
+  },
+  {
+    "revision": "b91aa1df1d639274e4c7",
+    "url": "/github.io/neighbors/static/js/main.fb6336cc.chunk.js"
+  },
+  {
+    "revision": "64deec72ca5d2c341643",
+    "url": "/github.io/neighbors/static/js/runtime-main.b8ae13a9.js"
+  }
+]);
